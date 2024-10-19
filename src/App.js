1a5238ad14a6ff1d49cd2aa8677687042aefb6ca
Hisham,Ali <his984@gmail.com>
2024-10-20 00:27:34 +0200

    Score Board

--- src/App.js
+++ src/App.js
@@ -5,18 +5,71 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Counters that update separately</h1>
-      <MyButton></MyButton>
-      <MyButton></MyButton>
+      <Scoreboard></Scoreboard>
     </div>
   );
 }
 export default App;
 
-function MyButton() {
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1);
+// Scoreboard
+function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Adam",
+    lastName: "Ali",
+    score: 1,
+  });
+  // handle Plus click
+  function handlePlusClick() {
+    setPlayer({
+      ...player,
+      score: player.score + 1,
+    });
   }
-  return <button onClick={handleClick}>Clicked {count} times</button>;
+  // Handle first name change
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+  // Handle last name change
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
+  }
+  return (
+    <fieldset>
+      <legend>
+        <h1>Score Board</h1>
+      </legend>
+      <div className="score">
+        <label className="score-counter">
+          Score: <b>{player.score}</b>
+          <button onClick={handlePlusClick}>+1</button>
+        </label>
+      </div>
+      <div className="person-info">
+        <div className="form-row">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            id="firstName"
+            type="text"
+            value={player.firstName}
+            onChange={handleFirstNameChange}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            id="lastName"
+            type="text"
+            value={player.lastName}
+            onChange={handleLastNameChange}
+          />
+        </div>
+      </div>
+    </fieldset>
+  );
 }
