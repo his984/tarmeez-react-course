1ef9755419f2b910b2612140c4ea073c06eceadd
Hisham,Ali <his984@gmail.com>
2024-10-20 11:47:43 +0200

    Add useState for name,phoneNumber and age

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
+// import Modal from "./Components/Modal";
 
 // APP
 function App() {
@@ -13,6 +14,11 @@ export default App;
 
 // Loan Form
 function LoanForm() {
+  const [loanInputs, setLoanInput] = useState({
+    name: "test",
+    phoneNumber: "",
+    age: "",
+  });
   return (
     // Loan form container
     <div className="loan-form">
@@ -24,27 +30,51 @@ function LoanForm() {
         <hr></hr>
         {/* Name */}
         <div className="form-row">
-          <label>Name: </label>
-          <input type="text"></input>
+          <label>Name * </label>
+          <input
+            type="text"
+            value={loanInputs.name}
+            onChange={(e) => {
+              setLoanInput({ ...loanInputs, name: e.target.value });
+            }}
+          ></input>
         </div>
         {/* Phone Number */}
         <div className="form-row">
-          <label>Phone number: </label>
-          <input type="text"></input>
+          <label>Phone number * </label>
+          <input
+            type="text"
+            value={loanInputs.phoneNumber}
+            onChange={(e) => {
+              setLoanInput({
+                ...loanInputs,
+                phoneNumber: e.target.value,
+              });
+            }}
+          ></input>
         </div>
         {/* Age */}
         <div className="form-row">
-          <label>Age: </label>
-          <input type="text"></input>
+          <label>Age * </label>
+          <input
+            type="text"
+            value={loanInputs.age}
+            onChange={(e) => {
+              setLoanInput({
+                ...loanInputs,
+                age: e.target.value,
+              });
+            }}
+          ></input>
         </div>
         {/* Are you employee  */}
         <div className="employee-check">
           <input type="checkbox"></input>
-          <label>Are you employee</label>
+          <label>Employee *</label>
         </div>
         {/* Salary */}
         <div className="select-salary">
-          <label>Salary: </label>
+          <label>Salary * </label>
           <select>
             <option>Option1</option>
             <option>Option2</option>
@@ -56,6 +86,8 @@ function LoanForm() {
         {/* Button */}
         <button>Submit</button>
       </form>
+      {/* Modal */}
+      {/* <Modal></Modal> */}
     </div>
   );
 }
---
+++ src/Components/Modal.js
@@ -0,0 +1,11 @@
+import "../App.css";
+
+export default function Modal() {
+  return (
+    <div id="modal">
+      <div id="modal-content">
+        <h2>The modal has been submitted successfully</h2>
+      </div>
+    </div>
+  );
+}
