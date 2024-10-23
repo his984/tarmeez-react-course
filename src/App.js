fa4fba6e166630a6419a89aae6f5f6933bbaad8e
Hisham,Ali <his984@gmail.com>
2024-10-23 03:16:13 +0200

    Applying useContext

--- src/App.js
+++ src/App.js
@@ -2,45 +2,53 @@ import { useState } from "react";
 import { places } from "./data.js";
 import { getImageUrl } from "./utils.js";
 import "./App.css";
+import { SizeContext, PlaceContext } from "./context/ListContext.js";
+import { useContext } from "react";
 
 // APP
 function App() {
   const [isLarge, setIsLarge] = useState(false);
   const imageSize = isLarge ? 150 : 100;
   return (
-    <div className="App">
-      <label>
-        <input
-          type="checkbox"
-          checked={isLarge}
-          onChange={(e) => {
-            setIsLarge(e.target.checked);
-          }}
-        />
-        Use large images
-      </label>
-      <hr />
-      <List imageSize={imageSize}></List>
-    </div>
+    <SizeContext.Provider value={imageSize}>
+      <div className="App">
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={(e) => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List imageSize={imageSize}></List>
+      </div>
+    </SizeContext.Provider>
   );
 }
 export default App;
 
 // List
 
-function List({ imageSize }) {
+function List() {
   const listItems = places.map((place) => (
-    <li key={place.id}>
-      <Place place={place} imageSize={imageSize} />
-    </li>
+    <PlaceContext.Provider value={place}>
+      <li key={place.id}>
+        <Place />
+      </li>
+    </PlaceContext.Provider>
   ));
   return <ul>{listItems}</ul>;
 }
 
-function Place({ place, imageSize }) {
+// Place
+function Place() {
+  const place = useContext(PlaceContext);
   return (
     <>
-      <PlaceImage place={place} imageSize={imageSize} />
+      <PlaceImage />
       <p>
         <b>{place.name}</b>
         {": " + place.description}
@@ -48,8 +56,10 @@ function Place({ place, imageSize }) {
     </>
   );
 }
-
-function PlaceImage({ place, imageSize }) {
+// PlaceImage
+function PlaceImage() {
+  const place = useContext(PlaceContext);
+  const imageSize = useContext(SizeContext);
   return (
     <img
       src={getImageUrl(place)}
---
+++ src/context/ListContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+export const SizeContext = createContext(100);
+export const PlaceContext = createContext(null);
