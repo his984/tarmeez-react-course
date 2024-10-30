e6a7391b3fb21ae178a8fab11b478b92282523da
Hisham,Ali <his984@gmail.com>
2024-10-31 00:55:45 +0100

    Get Started with Todo list project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
-import Posts from "./Components/Posts/Posts";
-import PostDetails from "./Components/postDetails/PostDetails";
-import MuButton  from "./MuButton"
+
 // APP
 function App() {
   return (
     <div className="root">
-      <div className="container">
-        {/* Navbar */}
-        <Navbar />
-        <MuButton/>
-        {/* Routes */}
-        <Routes>
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/post-details/:postId" element={<PostDetails />} />
-          <Route path="*" element={<h1>404 (Not Found !!)</h1>}></Route>
-        </Routes>
-      </div>f
+      <h1>Test</h1>
     </div>
   );
 }
